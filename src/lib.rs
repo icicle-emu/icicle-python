621572f42618675e28c8d3fb77ad9d3c870fb4aa
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2023-08-11 18:34:16 +0200

    Support Ghidra processor modules in the icicle python module directory

--- src/lib.rs
+++ src/lib.rs
@@ -515,6 +515,18 @@ class MemoryError(Exception):
         self.code = code
     def __str__(self):
         return f"{super().__str__()}: {self.code}"
+
+def __ghidra_init():
+    import os
+    for path in __path__ + [os.getenv("GHIDRA_SRC")]:
+        processors_dir = os.path.join(path, "Ghidra/Processors")
+        if os.path.isdir(processors_dir):
+            os.putenv("GHIDRA_SRC", path)
+            os.environ["GHIDRA_SRC"] = path
+            return
+    raise FileNotFoundError("Ghidra processors not found")
+
+__ghidra_init()
 "#, "icicle_exceptions.py", "icicle")?;
     Ok(())
 }
